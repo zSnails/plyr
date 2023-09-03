b8ad77b44c811fd7dd9200a5e21a9142209f3163
zSnails <erizojuan33@gmail.com>
2023-09-02 21:55:44 -0600

    Format SQL

--- creation.sql
+++ creation.sql
@@ -1,4 +1,10 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE songs (id integer primary key, title text, artist text, hash text unique, deleted bool);
+CREATE TABLE songs (
+    id integer primary key,
+    title text,
+    artist text,
+    hash text unique,
+    deleted bool
+);
 COMMIT;
