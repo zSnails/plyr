d8a3e1cb31f4ac9919547b428ecc49374c63610d
zSnails <erizojuan33@gmail.com>
2023-09-04 20:02:38 -0600

    Move ffmpegCommand into a global state variable
    This is in order for the variable to be used in the TUI.

--- eval.go
+++ eval.go
@@ -19,27 +19,6 @@ import (
 
 func eval(ctx context.Context, commandLine []string, reader *bufio.Reader) (err error) {
 
-	var ffmpegCommand = []string{
-		"-i",
-		"", // 1
-		"-c:a",
-		"libmp3lame",
-		"-b:a",
-		"128k",
-		"-map",
-		"0:0",
-		"-f",
-		"segment",
-		"-segment_time",
-		"10",
-		"-segment_list",
-		"outputlist.m3u8",
-		"-segment_format",
-		"mpegts",
-		"", // 16
-		//"output%03d.ts",
-	}
-
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
