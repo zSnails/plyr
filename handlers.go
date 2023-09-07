268730c1f6aeff3d9da9ff2313b83290c5c8c0b8
zSnails <erizojuan33@gmail.com>
2023-09-07 13:51:40 -0600

    feat(cache): Add caching for the REST API.
    Added caching for the restful api

--- handlers.go
+++ handlers.go
@@ -7,64 +7,47 @@ import (
 	"net/url"
 
 	"github.com/gorilla/mux"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 	log "github.com/sirupsen/logrus"
+	zslices "github.com/zSnails/plyr/slices"
+	"github.com/zSnails/plyr/storage"
 )
 
 func allSongs(w http.ResponseWriter, r *http.Request) {
 	log := log.WithContext(r.Context())
-	tx, rows, err := repo.All(r.Context())
-	if err != nil {
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer tx.Commit()
 
-	songs, err := makeSongDataSlice(rows)
-	if err != nil {
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	songs = filter(songs, func(sd SongData) bool {
+	songs := cache.All()
+
+	songs = zslices.Filter(songs, func(sd *storage.SongData) bool {
 		return !sd.Deleted
 	})
+
 	data, err := json.Marshal(songs)
 	if err != nil {
 		log.Error(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, "%s", data)
 }
 
 func songHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	songName, err := url.QueryUnescape(vars["songName"])
-	log := log.WithContext(r.Context()).WithField("song-name", songName)
-	if err != nil {
-		// fmt.Fprint(w, err)
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	tx, rows, err := repo.FindAlike(r.Context(), songName)
+	query, err := url.QueryUnescape(vars["songName"])
+	log := log.WithContext(r.Context()).WithField("song-name", query)
 	if err != nil {
 		log.Error(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	defer tx.Commit()
-	songs, err := makeSongDataSlice(rows)
-	if err != nil {
-		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	songs = filter(songs, func(sd SongData) bool {
-		return !sd.Deleted
+
+	songs := cache.Filter(func(sd *storage.SongData) bool {
+		return fuzzy.Match(removeSpecialCharacters(query), removeSpecialCharacters(sd.Title+sd.Artist)) ||
+			sd.Hash == query
 	})
+
 	data, err := json.Marshal(songs)
 	if err != nil {
 		log.Error(err)
